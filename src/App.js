cf0799fe4594cb774b487a17714339a988df0368
Tom wasserman <tomwas2000@gmail.com>
2025-05-25 15:39:01 +0300

    new

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/ask", {
+      const res = await fetch("https://travel-agent-backend-ztzn.onrender.com/ask", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
